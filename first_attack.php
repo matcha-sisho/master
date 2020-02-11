6b3815ff64f1121a7da5bc85b2be5cda9ddd3ab1
matcha sisho <matcha_sisho@yahoo.co.jp>
2020-02-11 19:54:08 +0900

    Add first attack script by php

---
+++ first_attack.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP‚Ìfirst attack</title>
+    </head>
+    <body>
+
+        <?php
+            echo "first attack";
+        ?>
+
+     </body>
+</html>
